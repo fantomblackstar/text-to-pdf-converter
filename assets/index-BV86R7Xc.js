08406436868532fd7656c0b7207a923178a84f72
fantomblackstar <71288130+fantomblackstar@users.noreply.github.com>
2025-05-17 20:32:43 +0300

    Updates

--- assets/index-BV86R7Xc.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Bphwv03n.js";import{a as s,H as t}from"./history-table-CDUpbqlU.js";const e=()=>r.jsx(s,{children:r.jsx(t,{})});export{e as default};
---
+++ assets/index-t9I0LYGk.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index--KXDTnY-.js";import{a as s,H as t}from"./history-table-DZq_LqTF.js";const e=()=>r.jsx(s,{children:r.jsx(t,{})});export{e as default};
